d598ad5a5a8d920a68835fe7a7a738e8b267efe8
Scott C. Farmer <32993780+ScottCF@users.noreply.github.com>
2018-10-12 05:59:51 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
+git commit -m "Added Hello before Holberton and changed 10 to 32 on line 5"
 int main()
 {
   int i;
-  for (i = 0; i < 10; i++)
+  for (i = 0; i < 32; i++)
   {
-    printf("Holberton\n");
+    printf("Hello Holberton\n");
   }
   return 0;
 }
